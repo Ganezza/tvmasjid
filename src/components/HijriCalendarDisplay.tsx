b48c66885abf62bcbfcc578e9f157db391c13b9b
[dyad] <git@dyad.sh>
2025-06-08 21:46:04 +0700

    [dyad] Menginstal dayjs-plugin-hijri dan memperbaiki jalur impor - wrote 3 file(s), added dayjs-plugin-hijri package(s)

--- src/components/HijriCalendarDisplay.tsx
+++ src/components/HijriCalendarDisplay.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import dayjs from "dayjs";
-import hijri from "dayjs/plugin/hijri"; // Reverted to standard Day.js plugin import path
+import hijri from "dayjs-plugin-hijri"; // Mengimpor langsung dari paket dayjs-plugin-hijri
 
 dayjs.extend(hijri);
 
