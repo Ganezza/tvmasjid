aa2bcada360aea39d3d97d6006f19eb438860094
Ganezza <63110406+Ganezza@users.noreply.github.com>
2025-06-10 21:17:07 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "/<YOUR_REPO_NAME>/", // Ganti dengan nama repositori Anda
+  base: "/MRLN/", // Ganti dengan nama repositori Anda
   server: {
     host: "::",
     port: 8080,
